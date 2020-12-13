05b7441c773bed88eae5d0cfac6df12c25f851eb
chiyo <ttl2132@columbia.edu>
2020-12-13 11:32:57 -0500

    finished schema, need to work on linking foreign keys

---
+++ mybinder/api/binder.py
@@ -0,0 +1,162 @@
+from flask import (
+    Blueprint, request, session, jsonify
+)
+from .app import db
+from .models import File, Object, Tag
+
+binder = Blueprint('binder', __name__, url_prefix='/binder')
+
+@binder.before_request
+def before_request():
+    try:
+        session['username']
+    except KeyError:
+        return "User not logged in.", 401
+
+@binder.route('/files', methods=['GET'])
+def files():
+    if request.method == 'GET':
+        files = File.query.with_entities(File.filename) \
+            .filter(File.username==session['username']).all()
+        names = [files[i][0] for i in range(len(files))]
+        return jsonify({'names': names}), 200
+
+
+@binder.route('/file/<name>', methods=['GET', 'POST', 'DELETE'])
+def myfile(name):
+    myfile = File.query.with_entities(File.filename) \
+            .filter(File.username==session['username'], File.filename==name).first()
+    if request.method == 'GET':
+        if myfile is None:
+            error = "File {} not found.".format(name)
+            return error, 400
+        return jsonify(file), 200
+    elif request.method == 'POST':
+        if myfile is None:
+            new_file = File(
+                filename=name,
+                username=session['username']
+            )
+            db.session.add(new_file)
+            db.session.commit()
+            return "File {} successfully created.".format(name), 200
+        print(myfile)
+        error = "File {} will be overwritten.".format(name)
+        return error, 400
+    elif request.method == 'DELETE':
+        if myfile:
+            File.query.filter(File.username==session['username'], File.filename==name).delete()
+            db.session.commit()
+            return "File {} successfully deleted.".format(name), 200
+        error = "File {} cannot be deleted as it does not exist.".format(name)
+        return error, 400
+
+@binder.route('/file/<filename>/objects', methods=['GET'])
+def objects(filename):
+    if request.method == 'GET':
+        objects = Object.query \
+            .filter(Object.username==session['username'], Object.filename==filename).all()
+        return jsonify({'objects': objects}), 200
+
+@binder.route('/file/<filename>/<page>/objects', methods=['GET'])
+def page_objects(filename, page):
+    if request.method == 'GET':
+        objects = Object.query \
+            .filter(Object.username==session['username'], Object.filename==filename, Object.page==page).all()
+        return jsonify({'objects': objects}), 200
+
+@binder.route('/file/<filename>/<page>/object/<name>', methods=['GET', 'POST', 'DELETE'])
+def myobject(filename, page, name):
+    myobject = Object.query \
+            .filter(
+                Object.username==session['username'],
+                Object.filename==filename,
+                Object.objectlink==name
+            ).first()
+    if request.method == 'GET':
+        if myobject is None:
+            error = "Object {} not found.".format(name)
+            return error, 400
+        return jsonify(myobject), 200
+    elif request.method == 'POST':
+        if myobject is None:
+            new_object = Object(
+                objectlink=name,
+                filename=filename,
+                username=session['username'],
+                page=page
+            )
+            db.session.add(new_object)
+            db.session.commit()
+            return "Object {} successfully created.".format(name), 200
+        print(myobject)
+        error = "Object {} will be overwritten.".format(name)
+        return error, 400
+    elif request.method == 'DELETE':
+        if myobject:
+            Object.query.filter(
+                Object.username==session['username'],
+                Object.filename==filename,
+                Object.objectlink==name
+            ).delete()
+            db.session.commit()
+            return "Object {} successfully deleted.".format(name), 200
+        error = "Object {} cannot be deleted as it does not exist.".format(name)
+        return error, 400
+
+@binder.route('/file/<filename>/tags', methods=['GET'])
+def tags(filename):
+    if request.method == 'GET':
+        tags = Tag.query \
+            .filter(Tag.username==session['username'], Tag.filename==filename).all()
+        return jsonify({'tags': tags}), 200
+
+@binder.route('/file/<filename>/tags/<tagtype>', methods=['GET'])
+def typed_tags(filename, tagtype):
+    if request.method == 'GET':
+        tags = Tag.query \
+            .filter(Tag.username==session['username'], Tag.filename==filename, Tag.tagtype==tagtype).all()
+        return jsonify({'tags': tags}), 200
+
+@binder.route('/file/<filename>/tags/<link1>/<link2>/<tagtype>', methods=['GET', 'POST', 'DELETE'])
+def tag(filename, link1, link2, tagtype):
+    tag = Tag.query \
+            .filter(
+                Tag.username==session['username'],
+                Tag.filename==filename,
+                Tag.objectlink==link1,
+                Tag.object2link==link2,
+                Tag.tagtype==tagtype
+            ).first()
+    if request.method == 'GET':
+        if tag is None:
+            error = "Tag between {} and {} not found.".format(link1, link2)
+            return error, 400
+        return jsonify(tag), 200
+    elif request.method == 'POST':
+        if tag is None:
+            new_tag = Tag(
+                objectlink=link1,
+                object2link=link2,
+                filename=filename,
+                username=session['username'],
+                tagtype=tagtype
+            )
+            db.session.add(new_tag)
+            db.session.commit()
+            return "Tag between {} and {} successfully created.".format(link1, link2), 200
+        error = "Tag between {} and {} will be overwritten.".format(link1, link2)
+        return error, 400
+    elif request.method == 'DELETE':
+        if tag:
+            Tag.query.filter(
+                Tag.username==session['username'],
+                Tag.filename==filename,
+                Tag.objectlink==link1,
+                Tag.object2link==link2,
+                Tag.tagtype==tagtype
+            ).delete()
+            db.session.commit()
+            return "Tag between {} and {} successfully deleted.".format(link1, link2), 200
+        error = "Tag between {} and {} cannot be deleted as it does not exist.".format(link1, link2)
+        return error, 400
--- mybinder/api/models.py
+++ mybinder/api/models.py
@@ -1,19 +1,22 @@
 from .app import db
 from dataclasses import dataclass
 
+
 @dataclass
 class Tag(db.Model):
     __tablename__ = 'tag'
 
     objectlink: str
     object2link: str
+    username: str
     filename: str
     tagtype: str
 
     objectlink = db.Column(db.String, db.ForeignKey('object.objectlink', ondelete='CASCADE'), primary_key=True)
     object2link = db.Column(db.String, primary_key=True)
-    filename = db.Column(db.String, db.ForeignKey('file.filename'))
-    tagtype = db.Column(db.String)
+    username = db.Column(db.String(50), db.ForeignKey('user.username', ondelete='CASCADE'), primary_key=True)
+    filename = db.Column(db.String, db.ForeignKey('file.filename', ondelete='CASCADE'), primary_key=True)
+    tagtype = db.Column(db.String, primary_key=True)
 
 @dataclass
 class Object(db.Model):
@@ -21,13 +24,22 @@ class Object(db.Model):
 
     objectlink: str
     filename: str
+    username: str
     page: int
     tags: Tag
 
     objectlink = db.Column(db.String, primary_key=True)
-    filename = db.Column(db.String, db.ForeignKey('file.filename', ondelete='CASCADE'))
+    filename = db.Column(db.String, primary_key=True)
+    username = db.Column(db.String(50), primary_key=True)
     page = db.Column(db.Integer)
-    tags = db.relationship('Tag', passive_deletes=True)
+    tags = db.relationship('Tag', backref='Object', passive_deletes=True)
+
+    __table_args__ = (
+        db.ForeignKeyConstraint(
+            ['filename', 'username'],
+            ['file.filename', 'file.username'],
+        ),
+    )
 
 @dataclass
 class File(db.Model):
@@ -39,7 +51,7 @@ class File(db.Model):
 
     username = db.Column(db.String(50), db.ForeignKey('user.username', ondelete='CASCADE'), primary_key=True)
     filename = db.Column(db.String, primary_key=True)
-    objects = db.relationship('Object', passive_deletes=True)
+    objects = db.relationship('Object', backref='File', passive_deletes=True)
 
 @dataclass
 class User(db.Model):
