a335f08ebdefffbbc54397ab689939ab6e947543
chiyo <ttl2132@columbia.edu>
2020-12-16 02:59:39 -0500

    Added logout button

--- mybinder/client/src/pages/DirectoryPage.jsx
+++ mybinder/client/src/pages/DirectoryPage.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Grid,
   GridList,
+  Snackbar,
   makeStyles,
   Typography,
 } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
 import { useHistory } from "react-router-dom";
 import logo from "../assets/mybind3r_logo.png";
 import DirectoryTable from "../components/DirectoryTable";
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 const DirectoryPage = () => {
   const history = useHistory();
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [alert, setAlert] = useState("");
+  const [severity, setSeverity] = useState("success");
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
 
   return (
     <div>
@@ -25,7 +41,31 @@ const DirectoryPage = () => {
             alt="Logo: Stick figure reading binder with 3r on the inside"
           />
         </Button>
-        <Button variant="outlined">Sign Out</Button>
+        <Button onClick={async () => {
+          await fetch("/auth/logout", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(),
+            }).then((value) => {
+              console.log(value.json()["message"]);
+              setAlert("Successful logout!");
+              setSeverity("success");
+              setOpen(true);
+              setTimeout(() => history.push("/"), 2000);
+            }).catch((error) => {
+              console.log("Error");
+            });
+          }} 
+          variant="outlined">
+            Sign Out
+        </Button>
+        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity={severity}>
+            {alert}
+          </Alert>
+        </Snackbar>
       </Grid>
       <Grid container direction="row">
         <Grid item xs container direction="column">
