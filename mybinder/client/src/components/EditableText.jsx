9c160795800ef1942878a88a910c711fb118b884
chiyo <ttl2132@columbia.edu>
2020-12-18 02:43:26 -0500

    Adding draggable text box

---
+++ mybinder/client/src/components/EditableText.jsx
@@ -0,0 +1,40 @@
+import React, { useState, Component } from "react";
+import Draggable from 'react-draggable';
+import {
+    TextField
+  } from "@material-ui/core";
+
+  class EditableText extends React.Component {
+    constructor(props) {
+        super(props);
+         this.state = {
+          text: "Note"
+        };
+    }
+    changeColor = (e) => {
+        this.setState({text: e.target.value});
+    }
+    render() {
+        return (
+        <Draggable>
+            <div>
+            <form noValidate>
+                <TextField
+                    //classname="apply-font"
+                    type='text'
+                    id="note"
+                    variant="outlined"
+                    multiline
+                    rows={2}
+                    rowsMax={Infinity}
+                    placeholder="Type..."
+                    onChange={this.changeColor}
+                    inputProps={{ disableUnderline: true }}
+                />
+            </form>
+            </div>
+        </Draggable>
+        );
+      }
+  }
+  export default EditableText;
