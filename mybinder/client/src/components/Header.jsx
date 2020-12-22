d776e8415c1347204fb2bbf00d6b113b7721a975
selena-huang <sh3696@columbia.edu>
2020-12-21 20:37:27 -0800

    FILE TOOLBAR WORKS :DDDD

--- mybinder/client/src/components/Header.jsx
+++ mybinder/client/src/components/Header.jsx
@@ -7,12 +7,8 @@ import Button from "@material-ui/core/Button";
 import PropTypes from "prop-types";
 import Box from "@material-ui/core/Box";
 import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { useHistory, useParams } from "react-router-dom";
 
 import FileToolbar from "./toolbar/FileToolbar";
 import HomeToolbar from "./toolbar/HomeToolbar";
@@ -84,8 +80,6 @@ function openTabs(index) {
 
 export default function Header() {
   const classes = useStyles();
-  const history = useHistory();
-  const { user } = useParams();
   const [value, setValue] = useState(0);
   const [state, setState] = useState({
     file: false,
@@ -93,12 +87,6 @@ export default function Header() {
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-  };
-
-  const handleFileItemClick = (value) => {
-    if (value === "Back to Scripts") {
-      history.push(`/${user}/directory`);
-    }
   };
 
   const toggleDrawer = (anchor, open) => (event) => {
@@ -116,8 +104,6 @@ export default function Header() {
     <div
       className={classes.drawer}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
     >
       <FileToolbar />
     </div>
