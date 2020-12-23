8dbf74f6531e0c0985c142d852577adc0f26c288
selena-huang <sh3696@columbia.edu>
2020-12-23 00:26:21 -0800

    Fixed ViewToolbar

--- mybinder/client/src/components/toolbar/ViewToolbar.jsx
+++ mybinder/client/src/components/toolbar/ViewToolbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Component } from "react";
+import React, {useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Tooltip from "@material-ui/core/Tooltip";
 import Grid from "@material-ui/core/Grid";
@@ -7,10 +7,10 @@ import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import Icon from "@material-ui/core/Icon";
 import IconButton from "@material-ui/core/IconButton";
 import IconDropdown from "../IconDropdown";
-import { Button, withStyles } from "@material-ui/core";
 import ComingSoon from "../ComingSoon";
+import { Typography } from "@material-ui/core";
 
-const useStyles = theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     height: 60,
     width: "fit-content",
@@ -24,115 +24,95 @@ const useStyles = theme => ({
   buttons: {
     padding: theme.spacing(1.5, 1),
   },
-  button: {
-    textTransform: 'none',
-    fontSize: "1rem",
-    fontFamily: "Roboto",
-    fontWeight: 400,
-    padding: "0px",
-    lineHeight: 1.5,
-    '&:hover': {
-      backgroundColor: "transparent",
-    },
-  },
   dropdown: {
     margin: theme.spacing(-0.5),
   },
-});
+}));
 
-class ViewToolbar extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      notated: 0,
-      dialogOpen: false
-    }
-  }
-
-  toggleDialog = () => {
-    this.state.dialogOpen= !this.state.dialogOpen;
+export default function ViewToolbar() {
+  const classes = useStyles();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const handleDialogClose = () => {
+    setDialogOpen(false);
   };
 
-  toggleLayer = () => {
-    if (this.state.notated == 0) {
-      this.state.notated = 100;
-    } else {
-      this.state.notated = 0;
-    }
-    console.log(this.state.notated);
-  };
-  render() {
-    const { classes } = this.props;
+  return (
+    <div>
+      <Grid container alignItems="center" className={classes.root}>
+        <Tooltip title="zoom-in">
+          <IconButton
+            className={classes.buttons}
+            color="inherit"
+            aria-label="zoom-in"
+            onClick={() => setDialogOpen(true)}
+          >
+            <Icon className="fas fa-search-plus" />
+          </IconButton>
+        </Tooltip>
 
-    return (
-      <div>
-        <Grid container alignItems="center" className={classes.root}>
-          <Tooltip title="zoom-in">
-            <IconButton
-              className={classes.buttons}
-              color="inherit"
-              aria-label="zoom-in"
-            >
-              <Icon className="fas fa-search-plus" />
-            </IconButton>
-          </Tooltip>
+        <Tooltip title="zoom-out">
+          <IconButton
+            className={classes.buttons}
+            color="inherit"
+            aria-label="zoom-out"
+            onClick={() => setDialogOpen(true)}
+          >
+            <Icon className="fas fa-search-minus" />
+          </IconButton>
+        </Tooltip>
 
-          <Tooltip title="zoom-out">
-            <IconButton
-              className={classes.buttons}
-              color="inherit"
-              aria-label="zoom-out"
-            >
-              <Icon className="fas fa-search-minus" />
-            </IconButton>
-          </Tooltip>
+        <Divider orientation="vertical" flexItem />
 
-          <Divider orientation="vertical" flexItem />
+        <Tooltip title="layers">
+          <IconDropdown
+            options={{
+              aria: "Page View",
+              items: [<div onClick={() => setDialogOpen(true)}>
+                  <Typography> Script-Only </Typography>
+              </div>, <div onClick={() => setDialogOpen(true)}>
+                  <Typography> Full Bind3r </Typography>
+              </div>],
+              icon: (
+                <div>
+                  <Icon className="fas fa-book-open" />
+                  &nbsp;Page View&nbsp;
+                  <ArrowDropDownIcon className={classes.dropdown} />
+                </div>
+              ),
 
-          <Tooltip title="layers">
-            <IconDropdown
-              options={{
-                aria: "Page View",
-                items: ["Script-Only", "Full Bind3r"],
-                icon: (
-                  <div>
-                    <Icon className="fas fa-book-open" />
-                    &nbsp;Page View&nbsp;
-                    <ArrowDropDownIcon className={classes.dropdown} />
-                  </div>
-                ),
-              }}
-            />
-          </Tooltip>
+            }}
+          />
+        </Tooltip>
 
-          <Tooltip title="layers">
-            <IconDropdown
-              options={{
-                aria: "layers",
-                items: [
-                  <Button disableRipple={true} className={classes.button} onClick={this.toggleDialog}>Rehearsal Script</Button>,
-                  <Button disableRipple={true} className={classes.button} onClick={this.toggleDialog}>Call Script</Button>,
-                  <Button disableRipple={true} className={classes.button} onClick={this.toggleDialog}>Tech-Tuesdays</Button>,
-                  <Button disableRipple={true} className={classes.button} onClick={this.toggleDialog}>+ New Layer</Button>,
-                ],
-                icon: (
-                  <div>
-                    <Icon className="fas fa-layer-group" />
-                    &nbsp;Layers&nbsp;
-                    <ArrowDropDownIcon className={classes.dropdown} />
-                  </div>
-                ),
-              }}
-            />
-          </Tooltip>
-        </Grid>
-        <ComingSoon onClose={this.toggleDialog} open={this.state.dialogOpen} />
-      </div>
-    );
-  }
+        <Tooltip title="layers">
+          <IconDropdown
+            options={{
+              aria: "layers",
+              items: [<div onClick={() => setDialogOpen(true)}>
+                <Typography> Rehearsal Script </Typography> </div>,
+                <div onClick={() => setDialogOpen(true)}>
+                <Typography> Call Script </Typography> </div>,
+                <div onClick={() => setDialogOpen(true)}>
+                <Typography> Tech-Tuesdays </Typography> </div>,
+                <div onClick={() => setDialogOpen(true)}>
+                <Typography> + New Layer </Typography> </div>,
+              ],
+              icon: (
+                <div>
+                  <Icon className="fas fa-layer-group" />
+                  &nbsp;Layers&nbsp;
+                  <ArrowDropDownIcon className={classes.dropdown} />
+                </div>
+              ),
+            }}
+          />
+        </Tooltip>
+      </Grid>
+      <ComingSoon onClose={handleDialogClose} open={dialogOpen} />
+    </div>
+  );
 }
 
-export default withStyles(useStyles)(ViewToolbar);
 
 /*
 
