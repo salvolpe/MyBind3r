549c7ffa04f18f77a9189daf7957358dbecd01ca
Salvatore Volpe <37944330+salvolpe@users.noreply.github.com>
2020-12-22 23:54:04 -0500

    Baseline work toward a splash screen

--- mybinder/client/src/pages/BinderPage.jsx
+++ mybinder/client/src/pages/BinderPage.jsx
@@ -8,63 +8,78 @@ import forwardArrow from "../assets/forward_arrow.png";
 import backArrow from "../assets/back_arrow.png";
 import sampleScript from "../assets/much_ado_3_1.png";
 import sampleScript2 from "../assets/much_ado_3_1_2.png";
+import LoadingScreen from "../components/LoadingScreen"; //currently not used
 
-
-class BinderPage extends React.Component {
+class BinderPage extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        numNotes: 0,
-        page: sampleScript
-      };
+    super(props);
+    this.state = {
+      numNotes: 0,
+      page: sampleScript,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.state.loading = false;
+    }, 6000);
   }
 
   addNote = () => {
-    this.setState({numNotes: this.state.numNotes + 1});
-  }
+    this.setState({ numNotes: this.state.numNotes + 1 });
+  };
 
   previousPage = () => {
-    this.setState({page: sampleScript});
-  }
+    this.setState({ page: sampleScript });
+  };
 
   nextPage = () => {
-    this.setState({page: sampleScript2});
-  }
+    this.setState({ page: sampleScript2 });
+  };
 
   render() {
     const notes = [];
     for (var i = 0; i < this.state.numNotes; i += 1) {
       notes.push(<EditableText />);
-    };
+    }
     console.log(notes.length);
     const arrowFooter = {
-          display: "flex",
-          height: 90,
-          borderBottom: "1px solid  #7C7C7C",
-          justifyContent: "space-between",
-          alignItems: "center",
-          paddingRight: "5.25%",
-          paddingLeft: "5.25%",
-        };
+      display: "flex",
+      height: 90,
+      borderBottom: "1px solid  #7C7C7C",
+      justifyContent: "space-between",
+      alignItems: "center",
+      paddingRight: "5.25%",
+      paddingLeft: "5.25%",
+    };
     const left = {
-          display: "flex",
-          alignItems: "center",
-        };
+      display: "flex",
+      alignItems: "center",
+    };
     return (
-
-      <Grid container justify="center" direction="column">
-        <Header />
-        <Grid container justify="center" direction="row">
-          <Binder page={this.state.page}/>
-        </Grid>
-        <Grid style={arrowFooter}>
-          <Grid style={left}>
-            <Button onClick={this.previousPage}><img src={backArrow}/></Button>
+      <>
+        {this.state.loading === false ? (
+          <Grid container justify="center" direction="column">
+            <Header />
+            <Grid container justify="center" direction="row">
+              <Binder page={this.state.page} />
+            </Grid>
+            <Grid style={arrowFooter}>
+              <Grid style={left}>
+                <Button onClick={this.previousPage}>
+                  <img src={backArrow} />
+                </Button>
+              </Grid>
+              <Button onClick={this.nextPage}>
+                <img src={forwardArrow} />
+              </Button>
+            </Grid>
           </Grid>
-          <Button onClick={this.nextPage}><img src={forwardArrow}/></Button>
-        </Grid>
-      </Grid>
-      
+        ) : (
+          <LoadingScreen />
+        )}
+      </>
     );
   }
 }
